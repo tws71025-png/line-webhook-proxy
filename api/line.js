cfdb6dc76f3b0ba749e5214fc977574713e9e388
tws71025-png <tws71025@gmail.com>
2025-08-31 05:00:01 +0800

    Update line.js

--- api/line.js
+++ api/line.js
@@ -3,7 +3,7 @@ export const config = { runtime: 'edge' };
 
 export default async function handler(req, ctx) {
   // ② 這裡換成你的 GAS /exec 網址（不是 /dev）
-  const GAS_EXEC = 'https://script.google.com/macros/s/AKfycbxDMm5Lztc6RnA58fapz7SHJ5oBRfS0ZhNwng4Yp4d1oPYZvrWRrkbPjzw2H9NyD5dJ/exec';
+  const GAS_EXEC = 'https://script.google.com/macros/s/AKfycbxWEp_OpL_hfTWYHjZF1gFwqvXutSRxAMAC1Ed-2Kepgfk4FqgOdmmkkiDMHlGdfTx1dA/exec';
 
   const res = new Response('OK', { status: 200, headers: { 'content-type': 'text/plain' } });
 
